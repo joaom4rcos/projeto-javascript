acbdf007ea2b2c93bea0554dca2db1b54520c544
joão marcos <jmjoao07nascimento@gmail.com>
2024-11-08 20:21:11 -0300

    Update Exercicio_N°1.js

--- Exercicio_N°1.js
+++ Exercicio_N°1.js
@@ -7,7 +7,7 @@ const taxaDolar = 5.50;
 let valorReais, valorConvertido;
 
 // Introdução
-console.log(`${"=".repeat(10)} Conversor de Real para Dólar ${"=".repeat(10)}\n`);
+console.log(`${"=".repeat(12)} Conversor de Reais para Dólar ${"=".repeat(12)}\n`);
 
 // Entrada de dados
 valorReais = parseFloat(prompt("Digite o valor em Reais (R$): "));
@@ -15,5 +15,6 @@ valorReais = parseFloat(prompt("Digite o valor em Reais (R$): "));
 // Processamento da conversão
 valorConvertido = valorReais / taxaDolar;
 
-// Saída formatada
+// Saída formatada com arredondamento
 console.log(`\nO valor R$${valorReais.toFixed(2)} convertido para dólar é aproximadamente: US$${valorConvertido.toFixed(2)}`);
+
