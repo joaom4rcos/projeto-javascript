0b52eed255f967fe9206ee7ba59a7413c4e6afe5
joão marcos <jmjoao07nascimento@gmail.com>
2024-11-08 20:56:18 -0300

    Update Exercicio-10.js

--- Exercicio-10.js
+++ Exercicio-10.js
@@ -1,35 +1,12 @@
-//inicio do algoritimo
 const prompt = require('prompt-sync')();
-console.clear();
+const { getFullYear, getDate, getMonth } = new Date();
 
-//variaveis
-const time = new Date()
-const ano = time.getFullYear();
-var idadeEmDias;
-var AnoNascimento;
-var mesNascimento = 0;
-var idade = [];
-var Data;
+// entrada
+console.log(`${"=".repeat(12)} Idade em dias ${"=".repeat(12)}\n`);
+let [dia, mes, ano] = prompt("Data de Nascimento (dd-mm-aaaa): ").split("-").map(Number);
 
-//entrada
-console.log(${"=".repeat(12)} Algoritimo que exibe sua idade em dias ${"=".repeat(12)}\n);
+// cálculo
+let idadeEmDias = (getFullYear() - ano) * 365.25 + (getMonth() - mes) * 30 + getDate() - dia;
 
-console.log("Digite sua data de nascimento com dia, mês e ano separados por '-' deste jeito: 00-00-0000\n");
-
-Data = prompt("Data de Nascimento: ");
-Data = Number((Data.split("-").reverse().join("")))
-
-//saida
-idade[0] = Number(String(Data).substring(0, 4));
-idade[1] = Number(String(Data).substring(4, 6));
-idade[2] = Number(String(Data).substring(6));
-
-AnoNascimento = ano - idade[0];
-
-for (i = idade[1]; i < 12; i++) {
-    mesNascimento++;
-}
-
-idadeEmDias = (AnoNascimento * 365.5) + (mesNascimento * 12) + idade[2];
-
-console.log(\nSua idade expressa em dias é aproximadamente: ${idadeEmDias.toFixed(0)} dias);
+// saída
+console.log(`\nSua idade em dias: ${idadeEmDias.toFixed(0)} dias`);
